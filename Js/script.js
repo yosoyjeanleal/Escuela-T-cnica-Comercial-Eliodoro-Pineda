4ff835cdacb64ae60bb194c847e283b637a37462
Jean Carlos Leal <139554701+yosoyjeanleal@users.noreply.github.com>
2024-11-04 23:32:12 -0400

    Delete Js directory

--- Js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.getElementById('hamburger');
-    const navLinks = document.getElementById('navLinks');
-
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-});
